86d720a416b4a5331f45eae462f5e365d7e681a2
David Asta <davidasta@gmail.com>
2022-11-18 19:06:20 +0100

    All working 100%

--- A3010KBD.ino
+++ A3010KBD.ino
@@ -1,7 +1,7 @@
 /* ---------------------------LICENSE NOTICE-------------------------------- 
  *  MIT License
  *  
- *  Copyright (c) 2019 David Asta
+ *  Copyright (c) 2019-2022 David Asta
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -27,23 +27,16 @@
   * 
   * CURRENT STATUS
   * ============================
-  *     \ | prints instead # ~
-  *     ` ~ prints instead ¬
-  *     2 @ prints instead "
-  *     3 # prints instead £
-  *     ' " prints instead ' @
-  *     
-  *     Not working:
-  *       It seems to work fine on Windows 10 and Linux Mint, but
-  *           on RISC OS Pi keeps repeating the key after releasing it.
-  *       Caps Lock LED not working well
-  *       Multiple modifier keys (e.g. Ctrl + Shift)
-  *       
-  *     Not implemented:
-  *       Scroll Lock LED
-  *       Num Lock LED
-  *       Pound key (I have to figure oput how to detect it)
-  *       # keypad (I have to figure oput how to detect it, and hthen I'll map it to the normal hash)
+  *     All keys, except pound and keypad # implemented.
+  *     All LEDs (CapsLock, ScrollLock, NumLock) implemented.
+  *     Power On LED is connected to +5V
+  *     Disk Activity LED is connected to a pin header, so it can be connected to adisk drive busy signal.
+  *
+  *     Tested successfully on:
+  *         Windows 10
+  *         Linux Mint 20.3 Cinnamon
+  *         Debian Bullseye (32-bit) on Raspberry Pi
+  *         RISC OS 5.28 on Raspberry Pi
   * ============================
   * 
   * The internal keyboard of the A3010 uses a membrane switch matrix 
@@ -211,16 +204,10 @@
 #error Something went wrong when including Keyboard.h
 #endif
 
-#define LED_BUILTIN 6     // LED on Teensy++ 2.0 will flash everytime time a key is pressed
-
-// Missing key definitions
-#define KEY_POUND   0x00
-#define KEYPAD_HASH 0x00
-
-//#define LED_BUILTIN 6     // LED on Teensy++ 2.0 flashes every key press
-#define NUM_ROWS 16   // TOP Connector: pins 1, 2, 3 and 4 are Not connected (NC)
-#define NUM_COLS 14   // BOTTOM Connector: pins 1, 17, 18, 19 and 20 are for LEDS, and pin 16 is +5Vcc
-#define DEBOUNCE_VALUE 40
+#define NUM_ROWS        16  // TOP Connector: pins 1, 2, 3 and 4 are Not connected (NC)
+#define NUM_COLS        14  // BOTTOM Connector: pins 1, 17, 18, 19 and 20 are for LEDS, and pin 16 is +5Vcc
+#define DEBOUNCE_VALUE  20  // As per my tests, 15 is the minimum value. Less than that, and keys 
+                            // start repeating on single presses. I left it as 20 to be in the safe side
 
 // Shift keys position in the matrix
 #define LSHIFT_COL 6
@@ -245,177 +232,160 @@
 #define CAPSLOCK_ROW 15
 
 // LEDs
-#define LED_CAPSLOCK 14    // C4
-#define LED_SCROLLLOCK 15  // C5
-#define LED_NUMLOCK 16     // C6
-#define LED_POWERON 45     // F7
-//#define LED_DISKACTIV      // Not used
+#define LED_CAPSLOCK    27
+#define LED_SCROLLLOCK  16
+#define LED_NUMLOCK     17
 
-// Pins for Teensy++ 2.0     0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
-// bottom cable -> columns (B7, D0, D1, D2, D3, D4, D5, D7, E0, E1, C0, C1, C2, C3)
-int colPins[NUM_COLS] =    {27,  0,  1,  2,  3,  4,  5,  7,  8,  9, 10, 11, 12, 13};
+//                          Pins on Teensy++ 2.0
+// bottom cable -> columns (D0, D1, D4, D5, D6, D7, E0, E1, C0, C1, C2, C3, C4, C5)
+int colPins[NUM_COLS] =    { 0,  1,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15};
 // top cable ----> rows    (F6, F5, F4, F3, F2, F1, F0, E6, E7, B0, B1, B2, B3, B4, B5, B6)
 int rowPins[NUM_ROWS] =    {44, 43, 42, 41, 40, 39, 38, 18, 19, 20, 21, 22, 23, 24, 25, 26};
 
+// Missing key definitions
+#define KEY_POUND   0x00
+#define KEYPAD_HASH 0x00
+
+// Key definitions
 unsigned int keyMap[NUM_ROWS][NUM_COLS] = {
-  {0, 0, 0, 0, 0, 0, KEY_RIGHT_ALT, 0, 0, 0, 0, 0, 0, 0}, // Row 0      pin 5
-  {0, 0, 0, 0, 0, 0, KEY_RIGHT_CTRL, 0, 0, 0, 0, 0, 0, 0}, // Row 1      pin 6
-  {0, 0, 0, 0, 0, 0, KEY_RIGHT_SHIFT, 0, 0, 0, 0, 0, 0, 0}, // Row 2      pin 7
-  {0, 0, 0, 0, 0, 0, KEY_PRINTSCREEN, 0, 0, 0, 0, 0, 0, 0}, // Row 3      pin 8
-  {KEY_SCROLL_LOCK, KEY_A, KEY_W, KEY_E, KEY_T, KEY_Y, 0, KEY_RETURN, KEY_UP, KEY_PAGE_DOWN, KEY_INSERT, KEY_RIGHT_BRACE, KEY_F12, KEY_F8}, // Row 4      pin 9
-  {KEY_PAUSE, KEY_Q, KEY_2, KEY_3, KEY_R, KEY_U, 0, KEY_I, 0, KEY_END, KEY_BACKSPACE, KEY_O, KEY_F11, KEY_F9}, // Row 5      pin 10
-  {0, 0, 0, 0, 0, 0, KEY_HOME, 0, 0, 0, 0, 0, 0, 0}, // Row 6      pin 11
-  {0, KEY_1, 0, 0, KEY_5, KEY_6, 0, KEY_F5, 0, 0, KEY_EQUAL, KEY_F6, KEY_F10, KEY_9}, // Row 7      pin 12
-  {0, 0, 0, 0, 0, 0, KEY_LEFT_SHIFT, 0, 0, 0, 0, 0, 0, 0}, // Row 8      pin 13
-  {0, 0, 0, 0, 0, 0, KEY_LEFT_CTRL, 0, 0, 0, 0, 0, 0, 0}, // Row 9      pin 14
-  {KEY_PAGE_UP, 0, KEY_F1, KEY_F4, KEY_4, KEY_7, 0, KEY_8, 0, KEY_DELETE, KEY_POUND, KEY_9, KEY_MINUS, 0}, // Row 10     pin 15
-  {KEY_NUM_LOCK, KEY_TAB, KEY_F2, KEY_F3, KEY_F, KEY_J, 0, KEY_BACKSLASH, KEYPAD_PERIOD, KEYPAD_HASH, KEYPAD_ASTERIX, KEY_L, KEYPAD_SLASH, KEY_0}, // Row 11     pin 16
-  {KEYPAD_7, KEY_Z, KEY_S, KEY_D, KEY_G, KEY_H, 0, KEY_K, KEY_RIGHT, KEYPAD_MINUS, KEYPAD_9, KEY_QUOTE, KEYPAD_8, KEY_LEFT_BRACE}, // Row 12     pin 17
-  {KEYPAD_4, KEY_TILDE, KEY_X, KEY_C, KEY_V, KEY_M, 0, KEY_COMMA, KEY_DOWN, KEYPAD_PLUS, KEYPAD_6, KEY_PERIOD, KEYPAD_5, KEY_P}, // Row 13     pin 18
-  {0, 0, 0, 0, 0, 0, KEY_LEFT_ALT, 0, 0, 0, 0, 0, 0, 0}, // Row 14     pin 19
-  {KEYPAD_1, KEY_ESC, KEY_CAPS_LOCK, KEY_SPACE, KEY_B, KEY_N, 0, KEY_LEFT, KEYPAD_0, KEYPAD_ENTER, KEYPAD_3, KEY_SLASH, KEYPAD_2, KEY_SEMICOLON} // Row 15     pin 20
+    /* Row 0  pin 5  */ {0, 0, 0, 0, 0, 0, KEY_RIGHT_ALT, 0, 0, 0, 0, 0, 0, 0},
+    /* Row 1  pin 6  */ {0, 0, 0, 0, 0, 0, KEY_RIGHT_CTRL, 0, 0, 0, 0, 0, 0, 0},
+    /* Row 2  pin 7  */ {0, 0, 0, 0, 0, 0, KEY_RIGHT_SHIFT, 0, 0, 0, 0, 0, 0, 0},
+    /* Row 3  pin 8  */ {0, 0, 0, 0, 0, 0, KEY_PRINTSCREEN, 0, 0, 0, 0, 0, 0, 0},
+    /* Row 4  pin 9  */ {KEY_SCROLL_LOCK, KEY_A, KEY_W, KEY_E, KEY_T, KEY_Y, 0, KEY_RETURN, KEY_UP, KEY_PAGE_DOWN, KEY_INSERT, KEY_RIGHT_BRACE, KEY_F12, KEY_F8},
+    /* Row 5  pin 10 */ {KEY_PAUSE, KEY_Q, KEY_2, KEY_3, KEY_R, KEY_U, 0, KEY_I, 0, KEY_END, KEY_BACKSPACE, KEY_O, KEY_F11, KEY_F9},
+    /* Row 6  pin 11 */ {0, 0, 0, 0, 0, 0, KEY_HOME, 0, 0, 0, 0, 0, 0, 0},
+    /* Row 7  pin 12 */ {0, KEY_1, 0, 0, KEY_5, KEY_6, 0, KEY_F5, 0, 0, KEY_EQUAL, KEY_F6, KEY_F10, KEY_9},
+    /* Row 8  pin 13 */ {0, 0, 0, 0, 0, 0, KEY_LEFT_SHIFT, 0, 0, 0, 0, 0, 0, 0},
+    /* Row 9  pin 14 */ {0, 0, 0, 0, 0, 0, KEY_LEFT_CTRL, 0, 0, 0, 0, 0, 0, 0},
+    /* Row 10 pin 15 */ {KEY_PAGE_UP, 0, KEY_F1, KEY_F4, KEY_4, KEY_7, 0, KEY_8, 0, KEY_DELETE, KEY_POUND, KEY_9, KEY_MINUS, 0},
+    /* Row 11 pin 16 */ {KEY_NUM_LOCK, KEY_TAB, KEY_F2, KEY_F3, KEY_F, KEY_J, 0, KEY_BACKSLASH, KEYPAD_PERIOD, KEYPAD_HASH, KEYPAD_ASTERIX, KEY_L, KEYPAD_SLASH, KEY_0},
+    /* Row 12 pin 17 */ {KEYPAD_7, KEY_Z, KEY_S, KEY_D, KEY_G, KEY_H, 0, KEY_K, KEY_RIGHT, KEYPAD_MINUS, KEYPAD_9, KEY_QUOTE, KEYPAD_8, KEY_LEFT_BRACE},
+    /* Row 13 pin 18 */ {KEYPAD_4, KEY_TILDE, KEY_X, KEY_C, KEY_V, KEY_M, 0, KEY_COMMA, KEY_DOWN, KEYPAD_PLUS, KEYPAD_6, KEY_PERIOD, KEYPAD_5, KEY_P},
+    /* Row 14 pin 19 */ {0, 0, 0, 0, 0, 0, KEY_LEFT_ALT, 0, 0, 0, 0, 0, 0, 0},
+    /* Row 15 pin 20 */ {KEYPAD_1, KEY_ESC, KEY_CAPS_LOCK, KEY_SPACE, KEY_B, KEY_N, 0, KEY_LEFT, KEYPAD_0, KEYPAD_ENTER, KEYPAD_3, KEY_SLASH, KEYPAD_2, KEY_SEMICOLON}
 };
 
 int debouncerCount[NUM_ROWS][NUM_COLS];
-bool capslock_on;
 
-
-
+/*****************************************************************************/
+/* Sets Teensy++ 2.0 for reading the matrix keyboard                         */
+/*****************************************************************************/
 void setup() {
-  // turn off Arduino on-board LED
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+    // set all LED pins as outputs and turn them OFF
+    pinMode(LED_CAPSLOCK,   OUTPUT); digitalWrite(LED_CAPSLOCK,   HIGH);
+    pinMode(LED_SCROLLLOCK, OUTPUT); digitalWrite(LED_SCROLLLOCK, HIGH);
+    pinMode(LED_NUMLOCK,    OUTPUT); digitalWrite(LED_NUMLOCK,    HIGH);
 
-  // set all LED pins as outputs and turn them OFF
-  pinMode(LED_CAPSLOCK,   OUTPUT); digitalWrite(LED_CAPSLOCK,   HIGH);
-  pinMode(LED_SCROLLLOCK, OUTPUT); digitalWrite(LED_SCROLLLOCK, HIGH);
-  pinMode(LED_NUMLOCK,    OUTPUT); digitalWrite(LED_NUMLOCK,    HIGH);
-  pinMode(LED_POWERON,    OUTPUT); digitalWrite(LED_POWERON,    HIGH);
-  //pinMode(LED_DISKACTIV,  OUTPUT); digitalWrite(LED_DISKACTIV,  HIGH);  // Not used
-  pinMode(LED_BUILTIN,    OUTPUT); digitalWrite(LED_BUILTIN,    LOW);
-
-
-  // set all ROWS pins as input pull-up
-  for(int r=0; r<NUM_ROWS; r++){
-    pinMode(rowPins[r], INPUT_PULLUP);
-  }
-
-  // set all COLS pins as inputs
-  for(int c=0; c<NUM_COLS; c++){
-    pinMode(colPins[c], INPUT);
-
-    // clear debouncer counts
+    // set all ROWS pins as input pull-up
     for(int r=0; r<NUM_ROWS; r++){
-      debouncerCount[r][c] = 0;
+        pinMode(rowPins[r], INPUT_PULLUP);
     }
-  }
 
-  capslock_on = false;
+    // set all COLS pins as inputs
+    for(int c=0; c<NUM_COLS; c++){
+        pinMode(colPins[c], INPUT);
 
-  // Switch ON the Power On LED
-  digitalWrite(LED_POWERON, LOW);
+        // clear debouncer counts
+        for(int r=0; r<NUM_ROWS; r++){
+            debouncerCount[r][c] = 0;
+        }
+    }
 }
 
+/*****************************************************************************/
+/* Main loop                                                                 */
+/* Reads the keyboard matrix to detect which key is pressed                  */
+/* and sends the key, at intervals, for as long as the key is pressed down   */
+/* Uses a debouncer to avoid the mechanical bouncing effect of keyboards     */
+/*****************************************************************************/
 void loop() {
-  readMatrixKBD();
+    int repeatCount = 0;
 
-  if(capslock_on){
-    digitalWrite(LED_CAPSLOCK, LOW);
-  }else{
-    digitalWrite(LED_CAPSLOCK, HIGH);
-  }
-}
+    // Check modifier keys (SHIFT, ALT, CTRL)
+    check_modifer_key(LSHIFT_ROW, LSHIFT_COL, KEY_LEFT_SHIFT);
+    check_modifer_key(RSHIFT_ROW, RSHIFT_COL, KEY_RIGHT_SHIFT);
+    check_modifer_key(LCTRL_ROW, LCTRL_COL,   KEY_LEFT_CTRL);
+    check_modifer_key(RCTRL_ROW, RCTRL_COL,   KEY_RIGHT_CTRL);
+    check_modifer_key(LALT_ROW, LALT_COL,     KEY_LEFT_ALT);
+    check_modifer_key(RALT_ROW, RALT_COL,     KEY_RIGHT_ALT);
 
-void check_modifer_key(int r, int c, int mod){
-  // is the modifier key pressed?
-  pinMode(colPins[c], OUTPUT);
-  digitalWrite(colPins[c], LOW);
-  pinMode(rowPins[r], INPUT_PULLUP);
-  
-  if(digitalRead(rowPins[r]) == LOW){
-    Keyboard.set_modifier(mod);
-  }
+    // for each column
+    for(int c=0; c<NUM_COLS; c++){
+        // enable column, by setting it as OUTPUT and LOW
+        pinMode(colPins[c], OUTPUT);
+        digitalWrite(colPins[c], LOW);
+      
+        // for each row
+        for(int r=0; r<NUM_ROWS; r++){
+            pinMode(rowPins[r], INPUT_PULLUP);
 
-  pinMode(rowPins[r], INPUT_PULLUP);
-  pinMode(colPins[c], INPUT);
-}
+            // if there is LOW on a row read, then a key was pressed
+            if(digitalRead(rowPins[r]) == LOW){
+                // apply a key debouncer to avoid the mechanical bouncing effect of keyboards
+                debouncerCount[r][c]++;
+                repeatCount = debouncerCount[r][c];
 
-void check_capslock(){
-  pinMode(colPins[CAPSLOCK_COL], OUTPUT);
-  digitalWrite(colPins[CAPSLOCK_COL], LOW);
-  pinMode(rowPins[CAPSLOCK_ROW], INPUT_PULLUP);
-  
-  if(digitalRead(rowPins[CAPSLOCK_ROW]) == LOW){
-    if(capslock_on){
-      capslock_on = false;
-    }else{
-      capslock_on = true;
+                if(repeatCount == 1){
+                    sendKey(r, c);
+                }else if(repeatCount > DEBOUNCE_VALUE){
+                    repeatCount -= DEBOUNCE_VALUE;
+
+                    if(repeatCount % (DEBOUNCE_VALUE * 2) == 0){
+                        sendKey(r, c);
+                    }
+                }
+            }else{
+                // clear debouncer count for ROW, COLUMN
+                debouncerCount[r][c] = 0;
+            }
+
+            // disable the row, by setting it as INPUT_PULLUP
+            pinMode(rowPins[r], INPUT_PULLUP);
+        }
+
+        // disable the column, by setting it as INPUT
+        pinMode(colPins[c], INPUT_PULLUP);
     }
-  }
-
-  pinMode(rowPins[CAPSLOCK_ROW], INPUT_PULLUP);
-  pinMode(colPins[CAPSLOCK_COL], INPUT);
 }
 
-void readMatrixKBD(){
-  int repeatCount = 0;
-  check_modifer_key(LSHIFT_ROW, LSHIFT_COL, MODIFIERKEY_SHIFT); // is the Left Shift pressed?
-  check_modifer_key(RSHIFT_ROW, RSHIFT_COL, MODIFIERKEY_SHIFT); // is the Right Shift pressed?
-  check_modifer_key(LCTRL_ROW, LCTRL_COL, MODIFIERKEY_CTRL); // is the Left Ctrl pressed?
-  check_modifer_key(RCTRL_ROW, RCTRL_COL, MODIFIERKEY_CTRL); // is the Right Ctrl pressed?
-  check_modifer_key(LALT_ROW, LALT_COL, MODIFIERKEY_ALT); // is the Left Alt pressed?
-  check_modifer_key(RALT_ROW, RALT_COL, MODIFIERKEY_ALT); // is the Right Alt pressed?
-
-  check_capslock(); // Not reliable. Sometimes stays on when it shouldn't
-
-  // ToDo - Multiple modifier keys (e.g. Ctrl+Shift) can be pressed at same time
-  //        but this implementation it isn't allowed because check_modifer_key()
-  //        set modifier individually, but it should do something like for 
-  //        example: Keyboard.set_modifier(MODIFIERKEY_CTRL | MODIFIERKEY_ALT);
-
-  // for each column
-  for(int c=0; c<NUM_COLS; c++){
-    // enable column, by setting it as OUTPUT and LOW
+/*****************************************************************************/
+/* Checks if any (Left or Right) of the Shift, Ctrl or Alt keys is pressed   */
+/* Sets (TRUE if pressed) global boolean variables for each key              */
+/*****************************************************************************/
+void check_modifer_key(int r, int c, int mod){
+    // is the modifier key pressed?
     pinMode(colPins[c], OUTPUT);
     digitalWrite(colPins[c], LOW);
-      
-    // for each row
-    for(int r=0; r<NUM_ROWS; r++){
-      pinMode(rowPins[r], INPUT_PULLUP);
+    pinMode(rowPins[r], INPUT_PULLUP);
 
-      // if there is LOW on a row read, then a key was pressed
-      if(digitalRead(rowPins[r]) == LOW){
-        // apply a key debouncer to avoid the mechanical bouncing effect of keyboards
-        debouncerCount[r][c]++;
-        repeatCount = debouncerCount[r][c];
-
-        if(repeatCount == 1){
-          digitalWrite(LED_BUILTIN, HIGH);
-          Keyboard.press(keyMap[r][c]);
-          Keyboard.release(keyMap[r][c]);
-        }else if(repeatCount > DEBOUNCE_VALUE){
-          repeatCount -= DEBOUNCE_VALUE;
-
-          if(repeatCount % (DEBOUNCE_VALUE * 2) == 0){
-            digitalWrite(LED_BUILTIN, HIGH);
-            Keyboard.press(keyMap[r][c]);
-            Keyboard.release(keyMap[r][c]);
-          }
-        }
-      }else{
-        // clear debouncer count for ROW, COLUMN
-        debouncerCount[r][c] = 0;
-      }
-
-      // disable the row, by setting it as INPUT_PULLUP
-      pinMode(rowPins[r], INPUT_PULLUP);
-      digitalWrite(LED_BUILTIN, LOW);
+    // If yes, send it
+    if(digitalRead(rowPins[r]) == LOW){
+        Keyboard.press(mod);
+    }else{
+        Keyboard.release(mod);
     }
 
-    // disable the column, by setting it as INPUT
-    pinMode(colPins[c], INPUT);
-  }
+    // disable the column and row
+    pinMode(rowPins[r], INPUT_PULLUP);
+    pinMode(colPins[c], INPUT_PULLUP);
+}
 
-  // Turn off Modifier keys (Ctrl, Shift, Alt, Windows Key
-  Keyboard.set_modifier(0);
+/*****************************************************************************/
+/* Sends the key value via USB                                               */
+/* Turns On/Off the LEDS, if the corresponding key was pressed               */
+/*****************************************************************************/
+void sendKey(int row, int col){
+    Keyboard.press(keyMap[row][col]);
+    Keyboard.release(keyMap[row][col]);
+
+    // Switch LEDs
+    if(keyMap[row][col] == KEY_CAPS_LOCK)
+        digitalWrite(LED_CAPSLOCK, !digitalRead(LED_CAPSLOCK));
+
+    if(keyMap[row][col] == KEY_NUM_LOCK)
+        digitalWrite(LED_NUMLOCK, !digitalRead(LED_NUMLOCK));
+
+    if(keyMap[row][col] == KEY_SCROLL_LOCK)
+        digitalWrite(LED_SCROLLLOCK, !digitalRead(LED_SCROLLLOCK));
 }
--- README.md
+++ README.md
@@ -1,52 +1,38 @@
-**Teensy++ 2.0 based keyboard controller for Acorn Archimedes A3010 keyboard to USB.**
+# Teensy++ 2.0 based keyboard controller for Acorn Archimedes A3010 keyboard to USB
 
 ![A3010 keyboard to USB](https://github.com/dasta400/A3010-USBkbd/blob/master/A3010kbd2USB.jpg "A3010 keyboard to USB")
 
 The Teensy board is connected to an Acorn Archimedes A3010 membrane switch matrix and acts as microcontroller and USB HID, thus allowing the connections of this keyboard to any computer through USB.
 
-## The Acorn Archimedes A3010
+## The Acorn Archimedes A3010 keyboard
+
 The A3010 keyboard has a key layout very similar to the industry standard PC keyboard. With 103 keys, 3 status LEDs (Caps Lock, Scroll Lock, Num Lock) and 2 extra LEDs (Power On, Disk Activity).
 
 On the original A3010 board the matrix was driven by a 87C51 8-bit microcontroller, except the Disk Activity LED. As the Teensy does not know anything about the disk activity on a computer, this LED can be used for another purpose. For now, I am not using it.
 
 ## Materials used
-- 1x Acorn Archimedes A3010 keyboard
-- 1x Teensy++ 2.0
-- 2x AMP 7-520355-0 connectors
-- For the LEDs
-  - 1x 220R carbon resistor 1/4 W
-  - 2x 270R carbon resistors 1/4 W
-  - 2x 330R carbon resistors 1/4 W
-- 1x Mini-b to A-Type cable
-- Soldering iron, solder, cable, pin headers and perfboard
 
-## Current status of this project
-- Wrong key mapping:
-  - \ | prints instead # ~
-  - ` ~ prints instead ¬
-  - 2 @ prints instead "
-  - 3 # prints instead £
-  - ' " prints instead ' @
+* 1x Acorn Archimedes A3010 keyboard
+* 1x Teensy++ 2.0
+* 2x AMP 7-520355-0 connectors
+* For the LEDs
+  * 1x 220R carbon resistor 1/4 W
+  * 2x 270R carbon resistors 1/4 W
+  * 2x 330R carbon resistors 1/4 W
+* 1x Mini-b to A-Type USB cable
+* Soldering iron, solder, cable, pin headers and perfboard
 
+## Current status
 
-- Not working:
-  - Caps Lock LED not working well. Sometimes stays on or off even the key has been pressed.
+* All keys, except pound and keypad # implemented.
+* All LEDs (CapsLock, ScrollLock, NumLock) implemented.
+* Power On LED is connected to +5V
+* Disk Activity LED is connected to a pin header, so it can be connected to a disk drive busy signal.
 
+## Tested systems
 
-- Not implemented:
-  - Multiple modifier keys (e.g. Ctrl + Shift). At the moment only one modifier key can be pressed in combination with another key.
-  - Scroll Lock LED
-  - Num Lock LED
-  - £ key (I have to figure out how to detect it)
-  - \# keypad (I have to figure out how to detect it, and then I'll map it to the normal hash)
-
-
-  ## Tested systems
-  - Reportedly works on:
-    - Linux Mint 17.3
-    - Windows 10 Home
-    - Rasberry Pi 2B with Amibian
-
-
-  - It does NOT work on:
-    - On Rasberry Pi 2B with RISC OS 5.24 keeps repeating the key after releasing it. I have read on the RISC OS OPEN forums ([this topic](https://www.riscosopen.org/forum/forums/4/topics/2383)) that this seems to be some sort of common issue with some keyboards. If anybody has a solution, please share.
+* Reportedly works (tested by me) on:
+  * Windows 10
+  * Linux Mint 20.3 Cinnamon
+  * Debian Bullseye (32-bit) on Raspberry Pi
+  * RISC OS 5.28 on Raspberry Pi
